457a6a17ba425b140cf3a2bc2d4dd81046322428
lexiofthefuture <alessiapaoletti00@gmail.com>
2026-01-18 18:29:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 //alert("script.js is connected");
 
-const trip = {
-  country: "Tanzania",
-  city: "Zanzibar",
-  month: "October",
-  year: 2025,
-  purpose: "Vacation",
-  rating: 5
-};
+const trips = [
+  {
+    country: "Tanzania",
+    city: "Zanzibar",
+    month: "October",
+    year: 2025,
+    purpose: "Vacation",
+    rating: 5
+  },
+  {
+    country: "Italy",
+    city: "Sardegna",
+    month: "September",
+    year: 2025,
+    purpose: "Vacation",
+    rating: 4
+  },
+   {
+    country: "Luxembourg",
+    city: "Luxembourg",
+    month: "October",
+    year: 2025,
+    purpose: "Friends",
+    rating: 5
+  },
+  {
+    country: "Spain",
+    city: "Alicante, Granada, Malaga",
+    month: "July",
+    year: 2025,
+    purpose: "Vacation",
+    rating: 5
+  }
+];
 
 function renderStars(rating) {
   let stars = "";
@@ -19,12 +45,16 @@ function renderStars(rating) {
 
 const tripList = document.getElementById("trip-list");
 
-tripList.innerHTML = `
-  <div class="trip-card">
-    <h2>${trip.city}, ${trip.country}</h2>
-    <p><strong>Period:</strong> ${trip.month} ${trip.year}</p>
-    <p><strong>Purpose:</strong> ${trip.purpose}</p>
-    <p><strong>Rating:</strong> ${renderStars(trip.rating)}</p>
-  </div>
-`;
+tripList.innerHTML = "";
+
+trips.forEach(function (trip) {
+  tripList.innerHTML += `
+    <div class="trip-card">
+      <h2>${trip.city}, ${trip.country}</h2>
+      <p><strong>Period:</strong> ${trip.month} ${trip.year}</p>
+      <p><strong>Purpose:</strong> ${trip.purpose}</p>
+      <p><strong>Rating:</strong> ${renderStars(trip.rating)}</p>
+    </div>
+  `;
+});
 
